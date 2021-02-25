5fc2b6327949d6794a2acb831e6068dd9eb787da
SKREPERIST <skreperist666@gmail.com>
2021-02-25 12:30:09 +0200

    added new-items.html

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(function(){
-
-
+// //////////////////////////////////////  
+// //////////////////////////////////////
   $(".rate-star").rateYo({
     rating: 5,
     starWidth: "12px",
@@ -8,7 +8,8 @@ $(function(){
     readOnly: true
   });
 
-
+// //////////////////////////////////////  
+// //////////////////////////////////////
 $('.product-slider__inner').slick({
   dots: true,
   arrows: false,
@@ -16,9 +17,40 @@ $('.product-slider__inner').slick({
   slidesToScroll: 4
 });
 
+// //////////////////////////////////////
+// //////////////////////////////////////
+$(".js-range-slider").ionRangeSlider({
+  type: "double",
+  min: 0,
+  max: 1000,
+  from: 0,
+  to: 600,
+  prefix: "$"
+}); 
+
+// //////////////////////////////////////
+// //////////////////////////////////////
+$('.icon-th-list').on('click', function(){
+  $('.product__item').addClass('list'),
+  $('.icon-th-large').removeClass('active'),
+  $('.icon-th-list').addClass('active')
+});
+
+$('.icon-th-large').on('click', function(){
+  $('.product__item').removeClass('list'),
+  $('.icon-th-list').removeClass('active'),
+  $('.icon-th-large').addClass('active')
+});
 
 
+// //////////////////////////////////////
+// //////////////////////////////////////
 var mixer = mixitup('.products__inner-box');
+
+
+
+
+
 
 
 
